4522f4c1f8a38bd99f32e6c4d758257e1d6dae56
devdanco <devdanco@gmail.com>
2025-04-04 14:26:06 +0200

    initial

--- src/theme.ts
+++ src/theme.ts
@@ -2,4 +2,11 @@ import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
   /** Put your mantine theme override here */
+  spacing: {
+    xs: '0.75rem',
+    sm: '1rem',
+    md: '1.5rem',
+    lg: '2rem',
+    xl: '2.5rem',
+  },
 });
