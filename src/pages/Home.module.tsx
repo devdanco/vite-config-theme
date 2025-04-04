e76d321c31d244e72b694f4e83898f0466f516fa
devdanco <devdanco@gmail.com>
2025-04-04 17:30:33 +0200

    initial

--- src/pages/Home.module.tsx
+++ src/pages/Home.module.tsx
@@ -1,11 +1,15 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import styles from './Home.module.scss';
 
 export function HomePage() {
   return (
-    <>
+    <div className={styles.container}>
+      <div className={styles.welcome}>
+        <h1>Hello world!</h1>
+      </div>
       <Welcome />
       <ColorSchemeToggle />
-    </>
+    </div>
   );
 }
